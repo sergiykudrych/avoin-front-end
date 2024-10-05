d6f387d2d393d5fc074ef4d65659abef4e592200
Sergiy Kudrych <81011171+sergiykudrych@users.noreply.github.com>
2024-10-05 13:24:01 +0300

    Update HomePage.js

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -20,6 +20,15 @@ const Home = () => {
   }, []);
   return (
     <div className="home">
+    <link href="https://api.cryptocloud.plus/static/widget/v2/css/app.css" rel="stylesheet" />
+    <vue-widget
+      shop_id="gQkiB6VYSU0Wb16G"
+      api_key="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiTWpZek1Eaz0iLCJ0eXBlIjoicHJvamVjdCIsInYiOiI1OTQzOTY0NjE0NWI3ZWQ1OWQxMDEyNjlmOTJlMWQyODllMzM5ZDZlZWY0MzhjZDI1ZGRlMTgwYmQ2MmQwY2UxIiwiZXhwIjo4ODEyODAzNjkzNX0.Ibgdr7ZAr6XhFPDckL4twqhf9X_EPwJQrEHi6zwZKSs"
+      currency="USD "
+      amount="50"
+      locale="ru"
+    ></vue-widget>
+    <script src="https://api.cryptocloud.plus/static/widget/v2/js/app.js"></script>
       <Hero />
       <Benefits />
       <Products />
